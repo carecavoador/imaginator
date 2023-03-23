51c49c7adb6d74e133173f08830ebbd9dbb92588
Careca Voador <e.rodrigo@outlook.com>
2023-03-23 14:49:24 -0300

    Calculando diferenças.

--- alinhar.py
+++ alinhar.py
@@ -2,17 +2,17 @@
 Practical Image Registration and Alignment with OpenCV and Python
 https://thinkinfi.com/image-alignment-and-registration-with-opencv/
 """
-
 import cv2
 import numpy as np
+
 
 # Imagem de referência
 img_referencia = cv2.imread("Entrada/original.jpg")
 
 # Imagem distorcida
 # img_distorcida = cv2.imread("Entrada/distorcido.jpg")
-# img_distorcida = cv2.imread("Entrada/distorcido_menor.jpg")
-img_distorcida = cv2.imread("Entrada/original_extra-01.jpg")
+img_distorcida = cv2.imread("Entrada/distorcido_menor.jpg")
+# img_distorcida = cv2.imread("Entrada/original_extra-01.jpg")
 
 
 # Converter para tons de cinza
@@ -22,17 +22,17 @@ altura, largura = img_ref_cinza.shape
 
 # Configure ORB feature detector Algorithm with 1000 features.
 orb_detector = cv2.ORB_create(1000)
- 
+
 # Extract key points and descriptors for both images
 keyPoint1, des1 = orb_detector.detectAndCompute(img_dist_cinza, None)
 keyPoint2, des2 = orb_detector.detectAndCompute(img_ref_cinza, None)
- 
+
 # Display keypoints for reference image in green color
 imgKp_Ref = cv2.drawKeypoints(img_referencia, keyPoint2, 0, (0,222,0), None)
 imgKp_Ref = cv2.resize(imgKp_Ref, (largura//2, altura//2))
- 
-cv2.imshow('Key Points', imgKp_Ref)
-cv2.waitKey(0)
+
+# cv2.imshow('Key Points', imgKp_Ref)
+# cv2.waitKey(0)
 
 
 # Match features between two images using Brute Force matcher with Hamming distance
@@ -53,17 +53,17 @@ no_of_matches = len(matches)
 imgMatch = cv2.drawMatches(img_dist_cinza, keyPoint2, img_ref_cinza, keyPoint1, matches[:100], None, flags = 2)
 imgMatch = cv2.resize(imgMatch, (largura//3, altura//3))
 
-cv2.imshow('Image Match', imgMatch)
-cv2.waitKey(0)
+# cv2.imshow('Image Match', imgMatch)
+# cv2.waitKey(0)
 
 # Define 2x2 empty matrices
 p1 = np.zeros((no_of_matches, 2))
 p2 = np.zeros((no_of_matches, 2))
 
 # Storing values to the matrices
-for i in range(len(matches)):
-    p1[i, :] = keyPoint1[matches[i].queryIdx].pt
-    p2[i, :] = keyPoint2[matches[i].trainIdx].pt
+for i, match in enumerate(matches):
+    p1[i, :] = keyPoint1[match.queryIdx].pt
+    p2[i, :] = keyPoint2[match.trainIdx].pt
 
 # Find the homography matrix.
 homography, mask = cv2.findHomography(p1, p2, cv2.RANSAC)
@@ -71,14 +71,15 @@ homography, mask = cv2.findHomography(p1, p2, cv2.RANSAC)
 # Use homography matrix to transform the unaligned image wrt the reference image.
 aligned_img = cv2.warpPerspective(img_distorcida, homography, (largura, altura))
 # Resizing the image to display in our screen (optional)
-aligned_img = cv2.resize(aligned_img, (largura//3, altura//3))
+# aligned_img = cv2.resize(aligned_img, (largura//3, altura//3))
+aligned_img = cv2.resize(aligned_img, (largura, altura))
 
 # Copy of input image
 imgTest_cp = img_dist_cinza.copy()
 imgTest_cp = cv2.resize(imgTest_cp, (largura//3, altura//3))
 # Save the align image output.
-# cv2.imwrite('output.jpg', aligned_img)
- 
-cv2.imshow('Input Image', imgTest_cp)
-cv2.imshow('Output Image', aligned_img)
-cv2.waitKey(0)
+cv2.imwrite('saida.jpg', aligned_img)
+
+# cv2.imshow('Input Image', imgTest_cp)
+# cv2.imshow('Output Image', aligned_img)
+# cv2.waitKey(0)
---
+++ diferencas.py
@@ -0,0 +1,53 @@
+"""
+https://pyimagesearch.com/2017/06/19/image-difference-with-opencv-and-python/
+Image Difference with OpenCV and Python
+"""
+from skimage.metrics import structural_similarity as compare_ssim
+import imutils
+import cv2
+
+
+img_referencia = "Entrada/original.jpg"
+img_teste = "saida.jpg"
+
+# load the two input images
+imageA = cv2.imread(img_referencia)
+imageB = cv2.imread(img_teste)
+# convert the images to grayscale
+grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+
+ALTURA, LARGURA = grayA.shape
+
+# compute the Structural Similarity Index (SSIM) between the two
+# images, ensuring that the difference image is returned
+(score, diff) = compare_ssim(grayA, grayB, full=True)
+diff = (diff * 255).astype("uint8")
+print("SSIM: {}".format(score))
+
+# threshold the difference image, followed by finding contours to
+# obtain the regions of the two input images that differ
+thresh = cv2.threshold(diff, 0, 255,
+	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+# loop over the contours
+for c in cnts:
+    # compute the bounding box of the contour and then draw the
+    # bounding box on both input images to represent where the two
+    # images differ
+    (x, y, w, h) = cv2.boundingRect(c)
+    cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+# Resize images to display
+imageA = cv2.resize(imageA, (ALTURA//3, LARGURA//3))
+
+# show the output images
+cv2.imshow("Original", imageA)
+cv2.imshow("Modified", imageB)
+cv2.imshow("Diff", diff)
+cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
